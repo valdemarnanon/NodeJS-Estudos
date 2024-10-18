4d92c81883d7d969b24c487a4a5c5a4fab7e34c3
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-18 14:55:28 -0300

    Mod app etc

---
+++ aula002 continuação/app.js
@@ -0,0 +1,21 @@
+const Cachorro = require('./z/mod2.js');
+
+const c1 = new Cachorro('CaT');
+c1.latir();
+
+
+
+// const path = require('path');
+// console.log(__dirname);
+// console.log(path.resolve(__dirname));
+
+
+// console.log(__filename); //nome do caminho
+// console.log(__dirname); //nome da pasta
+
+
+// const Cachorro = require('./z/mod2');
+
+// const dog = new Cachorro('Ted');
+
+// dog.latir();
---
+++ aula002 continuação/z/mod2.js
@@ -0,0 +1,3 @@
+const Cachorro = require('../b/c/d/mod1');
+
+module.exports = Cachorro;
