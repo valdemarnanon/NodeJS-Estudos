bb6cbd1bb17a9cc688e836fd13644f301b1c124d
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-19 16:48:43 -0300

    xd

--- aula002 continuação/app.js
+++ aula002 continuação/app.js
@@ -5,17 +5,18 @@ c1.latir();
 
 
 
-// const path = require('path');
-// console.log(__dirname);
-// console.log(path.resolve(__dirname));
+const path = require('path');
+console.log(__dirname);
+console.log(path.resolve(__dirname));
 
 
-// console.log(__filename); //nome do caminho
-// console.log(__dirname); //nome da pasta
+console.log(__filename); //nome do caminho
+console.log(__dirname); //nome da pasta
 
 
-// const Cachorro = require('./z/mod2');
+const Cachorro = require('./z/mod2');
 
-// const dog = new Cachorro('Ted');
+const dog = new Cachorro('Ted');
 
-// dog.latir();
+dog.latir();
+
