48b448a3229ba088024d0c1e031f3a669f5aa251
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-26 14:08:47 -0300

    x

--- aula015/server.js
+++ aula015/server.js
@@ -11,6 +11,11 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     console.log(e);
   });
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware');
@@ -18,6 +23,19 @@ const { middlewareGlobal } = require('./src/middlewares/middleware');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+  secret: 'iopsaijkdda812oikas',
+  store: new MongoStore({ mongooseConnection: mongoose.connection}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+})
+app.use(sessionOptions);
+app.use(routes);
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
