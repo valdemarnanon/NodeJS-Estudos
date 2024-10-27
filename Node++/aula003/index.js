b73226bacbff44ac162c2f79acb5755665788d9b
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-27 15:03:04 -0300

    Express routes

---
+++ Node++/aula003/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Bem vindo ao meu app!</h1>');
+});
+
+app.get('/sobre', (req, res) => {
+    res.send('<h1>Sobre nós</h1>');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('<h1>Nosso blog</h1>');
+});
+
+app.get('/ola/:cargo/:nome/:cor', (req, res) => {
+    res.send("<h1> Olá " + req.params.nome + '</h1>' +
+         "<h2> Seu cargo é: " + req.params.cargo + '</h2>' +
+         "<h3> Sua cor favorita é: " + req.params.cor + '</h3>');
+});
+
+
+
+
+
+
+// sempre a ultima linha do codigo;
+app.listen(3333, () => {
+    // função de callback, manda uma msg quando inicia o servidor.
+    console.log("Servidor rodando na url: http://localhost:3333")
+});
+// localhost:3333
